fb5364629e7e862aaff51adfea17d5b8f5d7fc28
suhas991 <suhas4497@gmail.com>
2024-06-24 21:11:43 +0530

    final

--- load-more/src/components/Loadmore.jsx
+++ load-more/src/components/Loadmore.jsx
@@ -1,16 +1,17 @@
 import { useState,useEffect } from "react"
 import "./Loadmore.css"
+
 export default function Loadmore(){
     const [product,setProduct]=useState([]);
     const [loading,setLoading]=useState(false);
-    const [count,]=useState(0);
+    const [count,setCount]=useState(0);
     const [disableButton, setDisableButton] = useState(false);
 
     async function fetchProduct(){
      
         try{
             setLoading(true);
-            const response=await fetch(`https://dummyjson.com/products?limit=10&skip=${count === 0? 0 : count * 10}`);
+            const response=await fetch(`https://dummyjson.com/products?limit=10&skip=${count === 0 ? 0 : count * 10}`);
             const data = await response.json();
             
             if(data && data.products && data.products.length){
@@ -19,24 +20,26 @@ export default function Loadmore(){
                 )
                 setLoading(false);
             }
-            console.log(data);
-            setCount(count+1);
+            //console.log(data);
 
         }catch(e){
             console.log(e);
             setLoading(false);
-        }
-
-        if(loading){
-            return <h4>Loading data..! Please wait..!</h4>
-        }
-        
+        }   
     }
 
     useEffect(()=>{
         fetchProduct();
     },[count]);
+    
+    useEffect(() => {
+        if (product && product.length === 190) 
+            setDisableButton(true);
+      }, [product]);
 
+      if (loading) {
+        return <div>Loading data ! Please wait.</div>;
+      }  
 
     return( 
     <div className="container">
@@ -51,6 +54,10 @@ export default function Loadmore(){
             ))
             :null
         }
+    </div>
+    <div className='button-container'>
+    <button disabled={disableButton} onClick={()=>setCount(count=>count+1)}>Load More</button>
+    {disableButton ? <p>No more Products</p> : null}
     </div>
     </div>
     )
